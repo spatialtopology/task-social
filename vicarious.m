6a996c612f0931097de7cd7db6dbb40dc4dd2317
jungheejung <heejung.jung@colorado.edu>
2019-06-20 00:38:08 -0600

    update task code comments

--- vicarious.m
+++ vicarious.m
@@ -1,1 +1,27 @@
 % vicarious pain
+
+%
+%% 1. fixation jitter 0-4 set
+% jitter 0-4 sec
+% confirm with Tor
+
+
+%% 2. cue 1s
+% confirm 1s?
+% 10 random social bars
+
+%% 3. expectation rating
+% rating triangle linear BMRK5.
+% mouse slide
+% mouse select
+
+%% 4. fixation
+
+%% 5. vicarious pain
+% 8 trials of different videos
+% random selection
+
+%% 6. post evaluation rating
+% BMRK5.
+% mouse slide
+% mouse select
