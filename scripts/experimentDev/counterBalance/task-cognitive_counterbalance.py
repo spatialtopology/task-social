875930a73fa99418388253f97542de69387d231e
jungheejung <heejung.jung@colorado.edu>
2019-06-27 14:16:19 -0600

    cleaned up comments

--- scripts/experimentDev/counterBalance/task-cognitive_counterbalance.py
+++ scripts/experimentDev/counterBalance/task-cognitive_counterbalance.py
@@ -8,7 +8,7 @@ from itertools import groupby
 from sklearn.utils import shuffle
 
 def noConsecutiveShuffle(list_key, list_dict, consec_num):
-    # keyList = list(['same','diff', 'same', 'diff']*14)
+    # resource... https://discourse.psychopy.org/t/force-trial-order-reshuffle-until-a-constraint-is-met/2101
     random.shuffle(list_key)
     orderTest=False
 
@@ -24,23 +24,28 @@ def noConsecutiveShuffle(list_key, list_dict, consec_num):
     for thisKey in list_key:
         trialList.append(list_dict[thisKey].pop())
 
-    return trialList #this can be deleted, just for debugging purposes
-    # https://discourse.psychopy.org/t/force-trial-order-reshuffle-until-a-constraint-is-met/2101
+    return trialList
+
 def generateListKey(trial_freq):
-    list_stim1 = range(1,trial_freq+1) #+ range(15,22)
-    list_stim2 = range(trial_freq+1,trial_freq*2+1) #+ range(22,29)
+    list_stim1 = range(1,trial_freq+1) # e.g. range(1,8)
+    list_stim2 = range(trial_freq+1,trial_freq*2+1) # e.g. range(8,15)
     list_dict = {'same':list_stim1, 'diff':list_stim2}
     random.shuffle(list_stim1)
     random.shuffle(list_stim2)
     x = ['same','diff']
     list_key  = [item for item in x for i in range(trial_freq)]
     return list_key, list_dict
-def shuffleDataFrame(df_subset):
 
-    return df_counterbalanced
+def shuffleDataFrame(df_subset):
+    list_key, list_dict = generateListKey(trial_freq)
+    list_order = noConsecutiveShuffle(list_key, list_dict, 4)
+    # 7) sort based on shuffled sequence
+    list_order[:] =  [x - 1 for x in list_order]
+    cB = df_subset.reindex(list_order)
+    cB.reset_index(drop = True, inplace = True)
+    return cB
 
 # parameters ___________________________________________________________________
-
 version_total = 4
 condition_block = 2
 trial_freq = 7
@@ -50,15 +55,14 @@ saveDir = '/Users/h/Dropbox/Projects/socialPain/design'
 # ______________________________________________________________________________
 
 
-# 1. choose 14 numbers out of 48 and create stimuli columns
+# 1. choose 14 numbers out of 48 and create stimuli columns ____________________
 num_select = []
 num_select = random.sample(range(1,49),  14)
 num_sort = sorted(num_select)
 list_random = random.sample(num_sort, len(num_sort))
 list1 = list_random[:7]
-list2= list_random[7:]
+list2 = list_random[7:]
 list_newShuffle = list1*2 + list2*2
-# list_newShuffle = list(np.array_split(list_random, 2)[list_ver])*2
 df = pd.DataFrame(list_newShuffle)
 df.columns = ['stimuli_num']
 
@@ -72,6 +76,7 @@ df['match'] = np.repeat(['same', 'different', 'same', 'different'], 7) # add col
 df['cue_type'] = np.resize(['high', 'low'], 28)
 df['random_order'] = range(len(df))
 df_reset = df.reset_index()
+
 # 5. calculated trial type _____________________________________________________
 df['trial_type'] = ((df.cue_type == 'low') & (df.degree == 50)).astype(int) * 1 + \
     ((df.cue_type == 'high') & (df.degree == 50)).astype(int) * 2 + \
@@ -97,19 +102,9 @@ for cB_ver in range(1,counterbalance_freq):
     random_other = np.setdiff1d(range(28),random_total)
     df_subset2 = df.loc[random_other]
     df_subset2.reset_index(drop = True, inplace = True)
-    # 2) shuffle each bin with the function  _______________________________________
+    # 2) shuffle each bin with the function  ___________________________________
     for index, df_subset in enumerate([df_subset1, df_subset2]):
-        # cB = shuffleDataFrame(df_loop)
-
-        list_key, list_dict = generateListKey(trial_freq)
-        list_order = noConsecutiveShuffle(list_key, list_dict, 4)
-        # 7) sort based on shuffled sequence
-        list_order[:] =  [x - 1 for x in list_order]
-        cB = df_subset.reindex(list_order)
-        cB.reset_index(drop = True, inplace = True)
-        # a) Add informative columns _________________________________________________
-        # condition_name, condition_num_filled_in_during_exper, block_num,
-        # cB_version, image_filename
+        cB = shuffleDataFrame(df_subset)
         cB['condition_name'] = np.repeat(['cognitive'], len(cB))
         cB['condition_num_filled_in_during_exper'] = 99
         cB['block_num'] = int(index+1)
@@ -128,19 +123,3 @@ for cB_ver in range(1,counterbalance_freq):
         'task-cognitive_counterbalance_ver-' + str('%02d' % cB_ver)+ \
          '_block-' +str('%02d' % int(index+1)) +'.csv'
         cB.to_csv(cBverFileName)
-
-# block 1  _____________________________________________________________________
-#
-# list_key, list_dict = generateListKey(trial_freq)
-# list_order = noConsecutiveShuffle(list_key, list_dict, 4)
-# # 7) sort based on shuffled sequence
-# list_order[:] =  [x - 1 for x in list_order]
-# df1 = df_subset1.reindex(list_order)
-# cB1 = df1.reset_index()
-# block 2  _____________________________________________________________________
-# list_key, list_dict = generateListKey(trial_freq)
-# list_order = noConsecutiveShuffle(list_key, list_dict, 4)
-# # 7) sort based on shuffled sequence
-# list_order[:] =  [x - 1 for x in list_order]
-# df2 = df_subset2.reindex(list_order)
-# cB2 = df2.reset_index()
