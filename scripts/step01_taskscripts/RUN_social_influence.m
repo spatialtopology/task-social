79d9b7eeed7c35b506f20a9b0e6460ca049b22c0
jungheejung <heejung.jung@colorado.edu>
2019-08-30 09:49:31 -0400

    added session number input

--- scripts/step01_taskscripts/RUN_social_influence.m
+++ scripts/step01_taskscripts/RUN_social_influence.m
@@ -1,7 +1,7 @@
 
 % 1. grab participant number ___________________________________________________
 prompt = 'subject number (in raw number form, e.g. 1, 2,...,98): ';
-sub = input(prompt)
+sub = input(prompt);
 
 
 
@@ -23,20 +23,20 @@ v2 = ['task-vicarious_counterbalance_ver-0' num2str(r_seq(index+1)) '_block-02']
 bl_ind = rem(sub,6);
 switch bl_ind
   case 0 % p1 v1 c2 c1 v2 p2
-    pain(p1);vicarious(v1);cognitive(c2);cognitive(c1);vicarious(v2);pain(p2);
+    pain(sub,p1,1);vicarious(sub,v1,2);cognitive(sub,c2,3);cognitive(sub,c1,4);vicarious(sub,v2,5);pain(sub,p2,6);
 
   case 1 % v1 c1 p1 p2 c2 v2
-    vicarious(v1);cognitive(c1);pain(p1);pain(p2);cognitive(c2);vicarious(v2);
+    vicarious(sub,v1,1);cognitive(sub,c1,2);pain(sub,p1,3);pain(sub,p2,4);cognitive(sub,c2,5);vicarious(sub,v2,6);
 
   case 2 % c1 p2 v1 v2 p1 c2
-    cognitive(sub,c1);pain(p2);vicarious(v1);vicarious(v2);pain(p1);cognitive(c2);
+    cognitive(sub,c1,1);pain(sub,p2,2);vicarious(sub,v1,3);vicarious(sub,v2,4);pain(sub,p1,5);cognitive(sub,c2,6);
 
   case 3 % p2 v2 c1 c2 v1 p1
-    pain(p2);vicarious(v2);cognitive(c1);cognitive(c2);vicarious(v1);pain(p1);
+    pain(sub,p2,1);vicarious(sub,v2,2);cognitive(sub,c1,3);cognitive(sub,c2,4);vicarious(sub,v1,5);pain(sub,p1,6);
 
   case 4 % v2 c2 p2 p1 c1 v1
-    vicarious(v2);cognitive(c2);pain(p2);pain(p1);cognitive(c1);vicarious(v1);
+    vicarious(sub,v2,1);cognitive(sub,c2,2);pain(sub,p2,3);pain(sub,p1,4);cognitive(sub,c1,5);vicarious(sub,v1,6);
 
   case 5 % c2 p1 v2 v1 p2 c1
-    cognitive(c2);pain(p1);vicarious(v2);vicarious(v1);pain(p2);cognitive(c1);
+    cognitive(sub,c2,1);pain(sub,p1,2);vicarious(sub,v2,3);vicarious(sub,v1,4);pain(sub,p2,5);cognitive(sub,c1,6);
 end
