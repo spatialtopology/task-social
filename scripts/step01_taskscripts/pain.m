82b5657c22305b727706e1ee3c74a1de551e81ab
jungheejung <heejung.jung@colorado.edu>
2019-08-30 23:21:39 -0400

    edited save file name format

--- scripts/step01_taskscripts/pain.m
+++ scripts/step01_taskscripts/pain.m
@@ -1,4 +1,4 @@
-function pain(sub,input_counterbalance_file)
+function pain(sub,input_counterbalance_file, run_num)
   
   %---------------------------------- ------------------------------------
   %                       Window Parameters
@@ -115,8 +115,8 @@ function pain(sub,input_counterbalance_file)
 
   imageTexture = Screen('MakeTexture', p.ptb.window, imread(cueImage));
   Screen('DrawTexture', p.ptb.window, imageTexture, [], [], 0);
-  Screen('Flip',p.ptb.window);
-  % p2_cue(trl) = GetSecs; % save output
+  p2_cue(trl) = Screen('Flip',p.ptb.window);
+%   p2_cue(trl) = GetSecs; % save output
   WaitSecs(1)
 
   %-------------------------------------------------------------------------------
@@ -204,18 +204,18 @@ function pain(sub,input_counterbalance_file)
   %-------------------------------------------------------------------------------
   %                                   save parameter
   %-------------------------------------------------------------------------------
-  sub_save_dir = fullfile(main_dir, 'data', strcat('sub-', sprintf('%02d', sub)), 'beh' );
+  sub_save_dir = fullfile(main_dir, 'data', strcat('sub-', sprintf('%03d', sub)), 'beh' );
   if ~exist(sub_save_dir, 'dir')
       mkdir(sub_save_dir)
   end
   T = table(p1_fixationPresent,p1_jitter,p2_cue,p3_ratingPresent,...
   p3_ratingDecideOnset,p3_decisionRT,p4_fixationPresent,p4_jitter,p5_responseOnset,...
   p5_responseKey,p5_RT,p6_ratingPresent,p6_ratingDecideOnset,p6_decisionRT);
-  saveFileName = fullfile(sub_save_dir,[strcat('sub-', sprintf('%02d', sub)), '_task-',taskname,'_beh.csv' ]);
+  saveFileName = fullfile(sub_save_dir,[strcat('sub-', sprintf('%03d', sub)), '_task-',taskname,'_run-', sprintf('%02d', run_num),'_beh.csv' ]);
   writetable(T,saveFileName)
   % save mouse trajectory
   trajectory_table = rating_Trajectory;
-  traject_saveFileName = fullfile(sub_save_dir, [strcat('sub-', sprintf('%02d', sub)), '_task-',taskname,'_beh_trajectory.mat' ]);
+  traject_saveFileName = fullfile(sub_save_dir, [strcat('sub-', sprintf('%03d', sub)), '_task-',taskname,'_run-', sprintf('%02d', run_num),'_beh_trajectory.mat' ]);
   save(traject_saveFileName, 'rating_Trajectory');
 
   sca;
