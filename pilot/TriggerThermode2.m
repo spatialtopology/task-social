dc44bcd95b61d52c6096e312feda81a35a3eb90a
jungheejung <heejung.jung@colorado.edu>
2019-12-12 19:17:16 +0900

    remove pilot folder from git remote repo, leave in local repo

--- pilot/TriggerThermode2.m
+++
@@ -1,9 +0,0 @@
-function [t] = TriggerThermode2(temp)
-ip = '192.168.1.3';
-newtemp = temp+10;
-port = 20121;
-main(ip, port, 1, newtemp);
-% main(ip, port, 4, newtemp);
-t = GetSecs;
-% main(ip, port, 5, newtemp);
-end
