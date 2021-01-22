f9bb8ffbfc40970daf84d425ae98c91bdf31fa4f
jungheejung <heejung.jung@colorado.edu>
2021-01-21 20:17:42 -0500

    update function name

--- scripts/step01_taskscripts/biopac_matlab_linux.m
+++
@@ -1,9 +0,0 @@
-function [time] = biopac_linux_matlab(channel, channel_num, state_num)
-  if channel.biopac
-      channel.d.setFIOState(pyargs('fioNum', int64(channel_num), 'state', int64(state_num)))
-      time = GetSecs;
-  else
-      time = GetSecs;
-      return
-  end
-end
